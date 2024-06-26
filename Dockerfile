640fff799317b9b98828b71685481d296c4012e0
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2024-06-26 19:28:45 +0800

    Revert "fix"
    This reverts commit 9a915a5deb8ce3881f8d6c17b6f8922edb42955e.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM recipes-api-deps AS deps
 
-FROM ghcr.io/rodfersou/docker-nix:latest AS runner
+FROM nixos/nix:latest AS runner
 ENV INSIDE_DOCKER="true"
 COPY --from=deps /tmp/nix-store-closure /nix/store
 COPY . /app
--- flake.nix
+++ flake.nix
@@ -5,18 +5,18 @@
     nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils, poetry2nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        unstable = nixpkgs-unstable.legacyPackages.${system};
+        unstable = nixpkgs-unstable.legacyPackages.${system}.extend poetry2nix.overlays.default;
       in with pkgs; rec {
         devShell = mkShell {
           name = "dev-environment";
           nativeBuildInputs = [
-            bash
             python311
             unstable.isort
             unstable.mypy
@@ -47,6 +47,30 @@
             export PATH="$PWD/.venv/bin:$PATH"
           '';
         };
+        packages.app = unstable.poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
+          overrides = unstable.poetry2nix.defaultPoetryOverrides.extend(self: super: {
+            flasgger = super.flasgger.overridePythonAttrs(old: {
+              buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
+            });
+            icdiff = super.icdiff.overridePythonAttrs(old: {
+              buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
+            });
+            beeprint = super.beeprint.overridePythonAttrs(old: {
+              buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
+            });
+            pytest-beeprint = super.pytest-beeprint.overridePythonAttrs(old: {
+              buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
+            });
+            polyfactory = super.polyfactory.overridePythonAttrs(old: {
+              buildInputs = (old.buildInputs or [ ]) ++ [ super.hatchling ];
+            });
+            #pydantic-factories = super.pydantic-factories.overridePythonAttrs(old: {
+            #  buildInputs = (old.buildInputs or [ ]) ++ [ super.poetry ];
+            #});
+          });
+        };
+        defaultPackage = packages.app;
       }
     );
 }
