756b14e4a6e09f13c1e89f30c7d1c00875b292d9
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2024-06-27 12:28:35 +0800

    Fix debug flag

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os
+from distutils.util import strtobool
 
-DEBUG = bool(os.environ.get("DEBUG", "true"))
-INSIDE_DOCKER = bool(os.environ.get("INSIDE_DOCKER", "false"))
+DEBUG = bool(strtobool(os.environ.get("DEBUG", "true")))
+INSIDE_DOCKER = bool(strtobool(os.environ.get("INSIDE_DOCKER", "false")))
 ORM_URL = os.environ.get("ORM_URL", "mysql+pymysql://user:password@0.0.0.0/mydatabase")
 SPECS_ROUTE = os.environ.get("SPECS_ROUTE", "/")
